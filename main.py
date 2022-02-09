4f6ef246b9d2635f1af3994057e3ca0532a5ec44
Ivan Dorofeev <ivandorofeev@icloud.com>
2022-02-10 00:32:44 +0300

    main.py is finished. Implemented connection to the DB, request sending and nice layout for the return

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-from flask import Flask
-from flask import request, jsonify
-import json
+from flask import Flask, jsonify, request
+import sqlite3 as sl
+from tabulate import tabulate
 
 app = Flask(__name__)
 
@@ -22,14 +22,17 @@ def filter_data():
     if request_data:
         if 'select' in request_data and len(request_data['select']) != 0:
             select_query = 'SELECT '
-            for column in request_data['select']:
-                select_query += column + ', '
+            len_without_cpi = len(request_data['select']) - (1 if 'cpi' in request_data['select'] else 0)
+            for i in range(0, len_without_cpi):
+                select_query += request_data['select'][i] + ', '
+            if 'cpi' in request_data['select']:
+                select_query += 'spend/installs AS cpi, '
 
             # remove the last ', '
             select_query = select_query[:-2]
 
         if 'filter' in request_data and len(request_data['filter']) != 0:
-            filter_query = 'WHERE '
+            filter_query = ' WHERE '
             # json array with all the filter parameters
             filter_received = request_data['filter']
             # adding all the filters to the query
@@ -43,22 +46,28 @@ def filter_data():
                 filter_query += 'os is ' + filter_received['date_from'] + ' AND '
 
             # remove the last 'AND '
-            groupby_query = filter_query[:-4]
+            filter_query = filter_query[:-4]
 
         if 'groupby' in request_data and len(request_data['groupby']) != 0:
-            groupby_query = 'GROUP BY '
+            groupby_query = ' GROUP BY '
             for column in request_data['groupby']:
-                select_query += column + ', '
+                groupby_query += column + ', '
 
             # remove the last ', '
-            groupby_query = select_query[:-2]
+            groupby_query = groupby_query[:-2]
 
         if 'sort' in request_data and len(request_data['sort']) == 2:
-            sort_query = 'ORDER BY ' + request_data['sort']['column']
+            sort_query = ' ORDER BY ' + request_data['sort']['column']
             if request_data['sort']['asc'] == "false":
                 sort_query += ' DESC'
 
-    return ''.join(str(sort_query))
+    final_query = select_query + ' FROM dataset \n' + filter_query + groupby_query + sort_query
+    conn = sl.connect('AdjustTest.db')
+    cursor = conn.cursor()
+    cursor.execute(final_query)
+
+    print(final_query)
+    return tabulate(cursor.fetchall(), tablefmt='html')
 
 
 if __name__ == '__main__':
